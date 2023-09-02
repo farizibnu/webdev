75b7df02bce39e2ac4e4569ea827d8f41a2a0015
farizibnu <fariz2240@gmail.com>
2023-09-02 10:54:06 +0700

    update tugas1

--- mission2/tugas1/tugas1.js
+++ mission2/tugas1/tugas1.js
@@ -1,72 +1,98 @@
-const minusButtons = document.getElementsByClassName("minus");
-const plusButtons = document.getElementsByClassName("plus");
-const inputFields = document.getElementsByClassName("input");
-const removeButton = document.getElementById('remove-btn');
-const cartItemsContainer = document.getElementById('cart-items');
-const cartTotalSpan = document.getElementById('cart-total');
-const addItemButtons = document.querySelectorAll('.add-item-btn');
+const plusButtons = document.querySelectorAll('.plus');
+const minusButtons = document.querySelectorAll('.minus');
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+const quantityInputs = document.querySelectorAll('.quantity');
+const cartItemsList = document.getElementById('cart-items');
+const cartTotal = document.getElementById('cart-total');
+const cartSubTotal = document.getElementById('cart-subtotal');
+const cartTax = document.getElementById("cart-tax");
+const removeButton = document.getElementById("remove-btn");
 
-const cart = [];
-let total = 0;
+let cart = {};
+const taxRate = 0.11;
+const productPrices = [1400000, 2200000, 1200000, 30000000, 1800000, 2500000];
 
-for (let i = 0; i < minusButtons.length; i++) {
-    minusButtons[i].addEventListener('click', event => {
-        event.preventDefault();
-        const currentValue = Number(inputFields[i].value) || 0;
-        inputFields[i].value = currentValue - 1;
-        console.log("minus clicked");
-    });
-}
-
-for (let i = 0; i < plusButtons.length; i++) {
-    plusButtons[i].addEventListener('click', event => {
-        event.preventDefault();
-        const currentValue = Number(inputFields[i].value) || 0;
-        inputFields[i].value = currentValue + 1;
-        console.log("plus clicked");
-    });
-}
-
-addItemButtons.forEach(button => {
+plusButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        
-        const itemName = button.getAttribute('data-item');
-        const itemPrice = parseFloat(button.getAttribute('data-price'));
-
-        cart.push({
-           
-            name: itemName,
-            price: itemPrice
-        });
-        total += itemPrice;
-
-        updateCartUI();
+        let quantity = parseInt(quantityInputs[index].value);
+        quantity++;
+        quantityInputs[index].value = quantity;
     });
 });
 
-function updateCartUI() {
-    cartItemsContainer.innerHTML = '';
-    cart.forEach(item => {
-        const pict = document.createElement("IMG");
-        pict.setAttribute("src", "/images/blue-shoes.jpg");
-        pict.setAttribute("width", "300");
-        pict.setAttribute("height", "200");
-        pict.setAttribute("alt", "blue-shoes");
-        cartItemsContainer.appendChild(pict);
-
-        const itemDiv = document.createElement('div');
-        itemDiv.innerHTML = `${item.name} - $${item.price}<hr>`;
-        cartItemsContainer.appendChild(itemDiv);
+minusButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        let quantity = parseInt(quantityInputs[index].value);
+        if (quantity > 0) {
+            quantity--;
+            quantityInputs[index].value = quantity;
+        }
     });
-    cartTotalSpan.textContent = total.toFixed(2);
+});
+
+addToCartButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        const productName = document.querySelectorAll('.product')[index].getAttribute('data-product-name');
+        const productPrice = productPrices[index];
+        const productImage = document.querySelectorAll('.product')[index].getAttribute('data-product-image');
+        const quantity = parseInt(quantityInputs[index].value);
+
+        if (quantity > 0) {
+            if (cart[productName]) {
+                cart[productName].quantity += quantity;
+            } else {
+                cart[productName] = {
+                    price: productPrice,
+                    quantity: quantity,
+                    image: productImage,
+                };
+            }
+
+            updateCart();
+        }
+    });
+});
+
+function updateCart() {
+    cartItemsList.innerHTML = '';
+    let subtotal = 0;
+
+    for (const productName in cart) {
+        const product = cart[productName];
+        const listItem = document.createElement('li');
+        listItem.classList.add("no-bullets");
+        listItem.innerHTML = `
+                    <img src="${product.image}" alt="${productName}" width="80">
+                    <span class="bold">${productName}</span><br><span class="d-flex justify-content-between">Jumlah: ${product.quantity}<span class="bold">Harga: ${formatToRupiah(product.price * product.quantity)}</span></span>
+                <hr>`;
+        cartItemsList.appendChild(listItem);
+        subtotal += product.price * product.quantity;
+    }
+
+    const tax = subtotal * taxRate;
+    const total = subtotal + tax;
+
+    cartSubTotal.textContent = formatToRupiah(subtotal);
+    cartTax.textContent = formatToRupiah(tax);
+    cartTotal.textContent = formatToRupiah(total);
+}
+
+function formatToRupiah(number) {
+    return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR'}).format(number);
 }
 
 function removeFromCart() {
-    cartItemsContainer.innerHTML = '';
-    cartTotalSpan.textContent = 0;
+    cartItemsList.innerHTML = '';
+    cartSubTotal.textContent = 0;
+    cartTax.textContent = 0;
+    cartTotal.textContent = 0;
+    total = 0;
+    cart = {};
     alert('Items removed from cart!');
 }
 
 removeButton.addEventListener('click', () => {
-    removeFromCart();
+    if (cart !== 0) {
+        removeFromCart();
+    }
 });
